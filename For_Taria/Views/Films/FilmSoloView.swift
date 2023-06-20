8045beeb26f07680d257a4b03cf36fc227cc2a44
Jason Hillegass <jason.d.hillegass@gmail.com>
2023-06-20 16:55:40 -0400

    FilmSoloViewUpdate
    Added film poster images and added the images to the film cell

--- For_Taria/Views/Films/FilmSoloView.swift
+++ For_Taria/Views/Films/FilmSoloView.swift
@@ -11,13 +11,60 @@ struct FilmSoloView: View {
     let film: Film
     
     var body: some View {
-        VStack {
-            Text(film.title)
-                .font(.largeTitle)
-            Text("Episode: \(film.episodeID)")
-            Text("directed by: \(film.director)")
-            Text("released: \(film.releaseDate)")
+        HStack {
+            let imageString = selectCorrectFilmImageString(for: film.episodeID)
+            Image(imageString)
+                .resizable()
+                .scaledToFit()
+            VStack {
+                Text(film.title)
+                    .font(.title3)
+                Group {
+                    Text("Episode: \(film.episodeID)")
+                    Text("directed by: \(film.director)")
+                    Text("released: \(film.releaseDate)")
+                }
+                .font(.subheadline)
+                .multilineTextAlignment(.leading)
+            }
         }
+        .frame(height: 100.00)
+    }
+    
+    
+    func selectCorrectFilmImageString(for film: Int) -> String {
+        var imageString = ""
+        
+        switch film {
+        case 1:
+            imageString = films.episode_one.rawValue
+        case 2:
+            imageString = films.episode_two.rawValue
+        case 3:
+            imageString = films.episode_three.rawValue
+        case 4:
+            imageString = films.episode_four.rawValue
+        case 5:
+            imageString = films.episode_five.rawValue
+        case 6:
+            imageString = films.episode_six.rawValue
+        case 7:
+            imageString = films.episode_seven.rawValue
+        default:
+            imageString = ""
+        }
+        
+        return imageString
+    }
+    
+    private enum films: String {
+        case episode_one = "episode_one"
+        case episode_two = "episode_two"
+        case episode_three = "episode_three"
+        case episode_four = "episode_four"
+        case episode_five = "episode_five"
+        case episode_six = "episode_six"
+        case episode_seven = "episode_seven"
     }
 }
 
